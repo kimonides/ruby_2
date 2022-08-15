207f1784ecd572d61b897fa9fa071e021acd5e0c
kimonides <kimonide@gmail.com>
2022-08-15 16:50:41 +0100

    a

--- main.rb
+++ main.rb
@@ -40,6 +40,39 @@ get '/callback' do
 
   puts response
 
+  playlist_id = JSON.parse(response.body)["id"]
+
+  # https://api.spotify.com/v1/playlists/{playlist_id}/tracks
+  body = {"uris": ["spotify:track:5n8Aro6j1bEGIy7Tpo7FV7","spotify:track:3nndHObJ5j5wpSKhTVODDG","spotify:track:6bhBAfmiMdvfFFtY0Rp8dA"]}
+  response = RestClient.post('https://api.spotify.com/v1/playlists/%s/tracks' % [playlist_id], body.to_json, content_type: :json, accept: :json, Authorization: "Bearer #{auth_params["access_token"]}") {|response, request, result| response }
+
+  snapshot_id = JSON.parse(response.body)["snapshot_id"]
+  puts snapshot_id
+
+  body = {"range_start": 1, "insert_before": 3, "range_length": 1}
+
+  response = RestClient.put('https://api.spotify.com/v1/playlists/%s/tracks' % [playlist_id], body.to_json, content_type: :json, accept: :json, Authorization: "Bearer #{auth_params["access_token"]}") {|response, request, result| response }
+
+
+  response = RestClient.get('https://api.spotify.com/v1/playlists/%s/tracks?limit=50' % [playlist_id], content_type: :json, accept: :json, Authorization: "Bearer #{auth_params["access_token"]}") {|response, request, result| response }
+
+  track_uri = JSON.parse(response.body)["items"][-1]["track"]["uri"]
+
+
+  body = {
+    "tracks": [{ "uri": track_uri }]
+  }
+  # response = RestClient.delete('https://api.spotify.com/v1/playlists/%s/tracks' % [playlist_id], body.to_json, content_type: :json, accept: :json, Authorization: "Bearer #{auth_params["access_token"]}") {|response, request, result| response }
+  headers = {
+    Authorization: "Bearer #{auth_params["access_token"]}",
+    content_type: :json, 
+    accept: :json
+  }
+  response = RestClient::Request.execute(:method => 'delete', :url => 'https://api.spotify.com/v1/playlists/%s/tracks' % [playlist_id], :payload => body.to_json, :headers => headers) {|response, request, result| response }
+
+  puts response
+  # puts JSON.pretty_generate(JSON.parse(response.body)["items"])
+
   'Hello world!'
 end
 
@@ -58,8 +91,9 @@ Thread.new {
   browser.input(id: "login-username").set "kimonide@gmail.com"
   browser.input(id: "login-password").set "1223334444"
   browser.button(id: "login-button").click
-  # sleep(100)
   browser.button(data_testid: "auth-accept").click
+  sleep(1)
+  browser.close()
 }
 
 
